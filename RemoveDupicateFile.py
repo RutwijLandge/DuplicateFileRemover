3e53cc0d87a1b0e17caf8ef4f8618de0638add61
RutwijLandge <rutwijlandge2003@gmail.com>
2025-08-03 23:46:29 +0530

    Add files via upload

---
+++ RemoveDupicateFile.py
@@ -0,0 +1,165 @@
+import os
+import sys
+import time
+import hashlib   
+
+def CalculateChecksum(path , BlockSize = 1024):
+    fobj =  open(path , 'rb' ) 
+
+    hobj = hashlib.md5()    
+
+    buffer = fobj.read(BlockSize)  
+
+    while(len(buffer) > 0):        
+
+        hobj.update(buffer)
+        buffer = fobj.read(BlockSize)
+
+    fobj.close()
+
+    return hobj.hexdigest()
+
+def DirectoryWatcher(DirectoryName = "Marvellous"):
+
+    flag = os.path.isabs(DirectoryName)
+
+    if(flag == False):
+        DirectoryName = os.path.abspath(DirectoryName)
+
+    flag = os.path.exists(DirectoryName)
+
+    if(flag == False):
+        print("The path is invalid")
+        exit()
+
+    flag = os.path.isdir(DirectoryName)
+
+    if(flag == False):
+        print("Path is valid but the target is not a directory")
+        exit()
+
+    for FolderName , SubFolderNames, FileNames in os.walk(DirectoryName):
+        for fname in FileNames:
+            fname = os.path.join(FolderName,fname)
+            checksum = CalculateChecksum(fname)
+            print("File Name : ",fname)
+            print("CheckSum  :",checksum)
+            print("")
+
+
+    timestamp = time.ctime()
+
+    filename = "MarvellousLog%s.log" %(timestamp)
+    filename = filename.replace(" ","_")
+    filename = filename.replace(":","_")
+
+    fobj = open(filename,"w")
+
+    Border = "-"*54
+    
+
+    fobj.write(Border+"\n")
+    fobj.write("This is is created at \n"+timestamp+"\n")
+    fobj.write(Border+"\n")
+    
+
+
+def FindDuplicate(DirectoryName = "Marvellous"):  # Newly Added
+
+    flag = os.path.isabs(DirectoryName)
+
+    if(flag == False):
+        DirectoryName = os.path.abspath(DirectoryName)
+
+    flag = os.path.exists(DirectoryName)
+
+    if(flag == False):
+        print("The path is invalid")
+        exit()
+
+    flag = os.path.isdir(DirectoryName)
+
+    if(flag == False):
+        print("Path is valid but the target is not a directory")
+        exit()
+
+    Duplicate = {}
+
+    for FolderName , SubFolderNames, FileNames in os.walk(DirectoryName): #loop travel for each SubFolderNames
+        for fname in FileNames:                                         
+            fname = os.path.join(FolderName,fname)
+            checksum = CalculateChecksum(fname)
+
+            if checksum in Duplicate:
+                Duplicate[checksum].append(fname)
+
+            else:
+                Duplicate[checksum] = [fname]
+
+    return Duplicate
+            
+
+def DisplayResult(MyDict):
+    Result = list(filter(lambda x : len(x) > 1, MyDict.values()))
+
+    Count = 0
+
+    for Value in Result:
+        for SubValue in Value:
+            Count = Count + 1 
+            print(SubValue)
+        print("-----------------------------------------")
+        print("Value of Count is :",Count)
+        print("-----------------------------------------")
+        Count = 0
+
+
+def DeleteDuplicate(MyDict):
+    Result = list(filter(lambda x : len(x) > 1, MyDict.values()))
+
+    Count = 0
+    Cnt = 0
+
+    for Value in Result:
+        for SubValue in Value:
+            Count = Count + 1 
+            if(Count > 1):
+                print("Deleted file : ",SubValue)
+                os.remove(SubValue)
+                Cnt = Cnt + 1
+        Count = 0
+    print("Total Delelted File : ",Cnt)
+    
+def main():
+    Border = "-"*54
+    print(Border)
+    print("--------------- Marvellous Automation ----------------")
+    print(Border)
+
+    if(len(sys.argv) == 2):
+        if((sys.argv[1] == "--h") or (sys.argv[1] == "--H")):
+            print("This application is used to perform directory cleaning")
+            print("This is the directory automation script")
+
+        elif((sys.argv[1] == "--u") or (sys.argv[1] == "--U")):
+            print("Use the given script as ")
+            print("ScriptName.py  NameOfDirctory")
+            print("Please provide valid absolute path")
+
+        else:
+            Result = FindDuplicate(sys.argv[1])
+            DeleteDuplicate(Result)
+
+    else:
+        print("Invalid number of coomand line arguments")
+        print("Use the given flags as : ")
+        print("--h : Used to display the help")
+        print("--u : Used to display the usage") 
+
+    print(Border)
+    print("----------- Thank you for using our script -----------")
+    print("---------------- Marvellous Infosystems --------------")
+    print(Border)
+
+if __name__ == "__main__":
+    main()
